4240f1f97b52ab78382e1a2921024596c08155e2
Eddyfabbricatore <eddyfabbricatore2@gmail.com>
2023-09-11 15:47:41 +0200

    [add] .bg-green, .bg-yellow, .bg-red css [add] if % i

--- script.js
+++ script.js
@@ -8,4 +8,19 @@ for(let i = 0; i < 100; i++){
   box.append(i + 1);
 
   boxContainer.append(box);
+
+  if(!((i + 1) % 3)){
+    box.innerHTML = 'Fizz'
+    box.classList.add('bg-green');
+  }
+
+  if(!((i + 1) % 5)){
+    box.innerHTML = 'Buzz'
+    box.classList.add('bg-yellow');
+  }
+
+  if(!((i + 1) % 3) && !((i + 1) % 5)){
+    box.innerHTML = 'FizzBuzz'
+    box.classList.add('bg-red');
+  }
 }
